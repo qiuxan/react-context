74b7c604aceb44652c7736aa7c2da9856ea43b65
qiuxan <qiuxan@gmail.com>
2025-01-30 13:30:48 +1100

    Move file

--- react-context/src/NewContext.tsx
+++ react-context/src/NewContext.tsx
@@ -27,14 +27,14 @@ class ChildB extends React.Component {
     declare context: React.ContextType<typeof ctx>;
 
     render() {
-        return <p>
+        return <>
             <h3>ChildB start</h3>
             <br/>In Child B - from Context：a: {this.context?.a}, b:{this.context?.b}
             <button onClick={() => {
                 this.context?.changeA(this.context.a + 2);
             }}>Last grandChild component B  btn click to a+2</button>
             <h3>ChildB end</h3>
-        </p>
+        </>
     }
 }
 
